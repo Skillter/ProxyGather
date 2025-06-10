194df3b57c75706378403d63b97deca29f77d625
Skillter <57605954+Skillter@users.noreply.github.com>
2025-06-10 03:06:34 +0200

    Init

---
+++ getproxy/getproxy-install.bat
@@ -0,0 +1,13 @@
+call conda activate
+
+echo Installing proxyz with pip
+pip install -U proxyz
+
+echo installing mubeng with go
+go install -v github.com/mubeng/mubeng@latest
+
+echo Downloading ProxyScraper.py
+curl -o scrapers/ProxyScraper-original.py https://raw.githubusercontent.com/SIDDHU123M/Ultimate-Proxy-Scraper/refs/heads/main/ProxyScraper.py
+
+
+timeout 5
