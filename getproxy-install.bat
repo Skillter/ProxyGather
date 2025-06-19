1e45354fe6b73d57c12ded5e8f88e514fe687f9f
Skillter <57605954+Skillter@users.noreply.github.com>
2025-06-19 23:08:43 +0200

    Some changes

--- getproxy-install.bat
+++ getproxy-install.bat
@@ -1,5 +1,12 @@
 call conda activate
 
+pip install -r requirements.txt
+
+git clone https://github.com/sarperavci/CloudflareBypassForScraping
+pip install -r CloudflareBypassForScraping/requirements.txt
+type nul > CloudflareBypassForScraping/__init__.py
+timeout 3
+
 echo Installing proxyz with pip
 pip install -U proxyz
 
@@ -9,8 +16,8 @@ go install -v github.com/mubeng/mubeng@latest
 echo Downloading ProxyScraper.py
 curl -o scrapers/ProxyScraper-original.py https://raw.githubusercontent.com/SIDDHU123M/Ultimate-Proxy-Scraper/refs/heads/main/ProxyScraper.py
 
-git clone https://github.com/chill117/proxy-lists.git
-cd proxy-lists
-npm install
+@REM git clone https://github.com/chill117/proxy-lists.git
+@REM cd proxy-lists
+@REM npm install
 
 timeout 5
